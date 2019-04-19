9119bf7009aa350b3b1eb10f1b9bdef17c02101a
peperoschach <luiz.felipe_23@yahoo.com.br>
2019-04-19 00:36:03 -0300

    Component Header

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 import Logo from '../components/template/Logo'
@@ -10,6 +12,6 @@ export default props =>
   <div className="app">
     <Logo />
     <Nav />
-    <Main />
+    <Main icon="home" title="Início" subtitle="Simple Project CRUD ReactJs" />
     <Footer />
   </div>
