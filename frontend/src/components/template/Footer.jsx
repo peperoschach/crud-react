ef435a32d7d064f6563597bb8300b3f0f4b7c5f6
peperoschach <luiz.felipe_23@yahoo.com.br>
2019-04-19 00:43:04 -0300

    Component Footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props => 
   <footer className="footer">
-    Rodapé
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong> Pepe <span className="text-danger">WebDeveloper</span></strong>
+    </span>
   </footer>
