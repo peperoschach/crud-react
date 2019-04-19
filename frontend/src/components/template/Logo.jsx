a0690f667f51c9006338fa9280ca54b3658e6647
peperoschach <luiz.felipe_23@yahoo.com.br>
2019-04-18 23:46:51 -0300

    Component Logo

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,8 +1,11 @@
 import './Logo.css'
+import logo from '../../assets/images/pepelogo-1.png'
 import React from 'react'
 
 
 export default props => 
   <aside className="logo">
-    Logo
+    <a href="/" className="logo">
+      <img src={logo} alt="Logo" />
+    </a>
   </aside>
