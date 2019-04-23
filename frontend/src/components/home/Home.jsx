b5cf057f03745f50da37216d4a8d696c55dd70aa
peperoschach <luiz.felipe_23@yahoo.com.br>
2019-04-23 01:00:59 -0300

    Create functions Crud

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props => 
+  <Main icon="home" title="Início" subtitle="Simple Project CRUD ReactJs">
+    <div className="display-4">
+      Bem Vindo
+      </div>
+    <hr />
+    <p className="mb-0">Sistema simples de cadastro usando o ReactJs</p>
+  </Main>
